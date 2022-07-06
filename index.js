01b8d71fcbc0920723ddb68505a91faa32370104
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-07-06 15:18:46 -0500

    first steps

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+//make a network request to fetch json and print the result
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1["default"].get(url).then(function (response) {
+    console.log(response.data);
+});
---
+++ index.ts
@@ -0,0 +1,8 @@
+//make a network request to fetch json and print the result
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+  console.log(response.data);
+});
